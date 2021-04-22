8bbfefd22d3c81c6bc6fc48e71531556cff02267
Victor Vassilev <vic.vassilev@gmail.com>
2021-04-23 01:23:24 +0300

    More fixes

--- DeliverIt13/DeliverIt13.Services/Models/UserDTOs/UserCreatePublicDTO.cs
+++ DeliverIt13/DeliverIt13.Services/Models/UserDTOs/UserCreatePublicDTO.cs
@@ -16,7 +16,6 @@ namespace DeliverIt13.Services.Models.UserDTOs
             this.Email = user.Email;
         }
 
-        public int  UserId { get; set; }
         public string Email { get; set; }
         public UserType Type { get; set; }
         public string Password { get; set; }
